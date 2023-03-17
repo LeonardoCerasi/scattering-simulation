76e18770647ef9d99e4b94bb7f73cbb7a2d8ad2a
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2023-03-17 19:14:46 +0100

    Added function to read data to Input library

--- include/tools/input.h
+++ include/tools/input.h
@@ -2,7 +2,9 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 /**
  * @brief This function returns the number of lines in a file
@@ -11,3 +13,12 @@
  * @return (int) Number of lines
  */
 int lines(const char *path);
+
+/**
+ * @brief This function reads an input file of space-separated doubles and uploads them into a vector of vectors of doubles, one vector for each line of the file
+ *
+ * @param path Path of the input file
+ * @param type f: Framework / p: Particle
+ * @return (std::vector<std::vector<double>>) Vector of rows of doubles
+ */
+std::vector<std::vector<double>> read_data(const char *path, char type);
--- src/tools/input.cpp
+++ src/tools/input.cpp
@@ -26,3 +26,53 @@ int lines(const char *path)
 
     return lines;
 }
+
+std::vector<std::vector<double>> read_data(const char *path, char type)
+{
+    // number of lines in input file
+    int n = lines(path);
+
+    // number of doubles per row
+    int d = 0;
+    if (type == 'f')
+    {
+        d = 12;
+    }
+    else if (type == 'p')
+    {
+        d = 10;
+    }
+
+    std::ifstream input;
+    input.open(path);
+
+    if (!input)
+    {
+        std::cout << "Error during input file opening" << std::endl;
+        return {{-1}};
+    }
+
+    // initialize returned vector
+    std::vector<std::vector<double>> result;
+
+    for (int i = 0; i < n; i++)
+    {
+        // initialize support vector
+        std::vector<double> support;
+
+        // initialize support double
+        double value;
+
+        // frameworks are defined by 12 parameters
+        for (int j = 1; j <= d; j++)
+        {
+            input >> value;
+            support.push_back(value);
+        }
+
+        result.push_back(support);
+    }
+
+    input.close();
+    return result;
+}
