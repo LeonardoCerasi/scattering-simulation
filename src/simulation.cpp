518e7c048d5e5f6b9dd0e4fd580a9d0c33293d05
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2023-03-17 18:46:52 +0100

    Added test for Input library

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -2,9 +2,13 @@
 
 #include "Config.h"
 
+#include "input.h"
+
 int main()
 {
     std::cout << "Version " << Scattering_VERSION_MAJOR << "." << Scattering_VERSION_MINOR << std::endl;
+
+    std::cout << "Linee: " << lines("src/simulation.cpp") << std::endl;
 
     return 0;
 }
