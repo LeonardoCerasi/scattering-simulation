cc4f42535fc34aecc2927da0aa10dcf7f38f5a82
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2023-03-17 19:24:07 +0100

    Added test for Partcile class

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -3,12 +3,17 @@
 #include "Config.h"
 
 #include "tools/input.h"
+#include "classes/particle.h"
 
 int main()
 {
     std::cout << "Version " << Scattering_VERSION_MAJOR << "." << Scattering_VERSION_MINOR << std::endl;
 
-    std::cout << "Linee: " << lines("src/simulation.cpp") << std::endl;
+    std::cout << "Linee: " << lines("src/classes/particle.cpp") << std::endl;
+
+    Particle part = Particle();
+
+    std::cout << "Mass: " << part.get_mass() << std::endl;
 
     return 0;
 }
