929c0d6c731e3fccc384955835519f4a970b77cd
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2023-03-17 18:10:17 +0100

    Initialized CMake structure of the repository

---
+++ src/simulation.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "Config.h"
+
+int main()
+{
+    std::cout << "Version " << Scattering_VERSION_MAJOR << "." << Scattering_VERSION_MINOR << std::endl;
+
+    return 0;
+}
