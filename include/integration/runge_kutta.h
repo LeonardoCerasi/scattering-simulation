57fe6f4a2014a1b78f17a728e17094977aad2f94
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2024-03-09 22:57:03 +0100

    Added Runge-Kutta integration method

---
+++ include/integration/runge_kutta.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <functional>
+
+#include "arr_op.h"
+
+template <size_t N>
+class runge_kutta
+{
+    public:
+
+        std::array<double, N> step(double t, double h, const std::array<double, N> &x, std::function<std::array<double, N> (double, const std::array<double, N> &)> f) const override
+        {
+            std::array<double, N> k_1{f(t, x)};
+            std::array<double, N> k_2{f(t + h/2., x + k_1 * h/2.)};
+            std::array<double, N> k_3{f(t + h/2., x + k_2 * h/2.)};
+            std::array<double, N> k_4{f(t + h, x + k_3 * h)};
+
+            return (x + (k_1 + 2. * k_2 + 2. * k_3 + k_4) * h / 6.);
+        }
+};
